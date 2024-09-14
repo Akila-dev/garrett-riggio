3ab1e52476638ea04fb1d8da1b2e4790f33dbec1
unknown <akilapanunayan@gmail.com>
2024-09-14 10:26:39 +0100

    corrections

--- app/page.js
+++ app/page.js
@@ -1,11 +1,23 @@
 'use client';
-
+import { useSate, useEffect } from 'react';
 import { Footer } from '@/containers';
 
 export default function Home() {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setIsLoading(false);
+	}, []);
+
 	return (
 		<main className="">
-			<Footer />
+			{isLoading ? (
+				<div className="w-full h-screen flex items-center justify-center text-white text-base">
+					Loading...
+				</div>
+			) : (
+				<Footer />
+			)}
 		</main>
 	);
 }
--- components/index.js
+++ components/index.js
@@ -1,7 +1,5 @@
 export { default as ZoomingCardsBG } from './ZoomingCardsBG';
 
-export { default as Loader } from './Loader';
-
 export { default as WavyText } from './text/WavyText';
 export { default as ScrambleText } from './text/ScrambleText';
 export { default as ClipHeightText } from './text/ClipHeightText';
